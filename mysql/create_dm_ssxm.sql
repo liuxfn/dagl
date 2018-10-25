3040c149d986c65de4fc2c27638261f85313c151
lxf <hndz.lxf@qq.com>
2018-10-26 00:03:27 +0800

    新增dm_ssxm脚本

---
+++ mysql/create_dm_ssxm.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `dagl`.`dm_ssxm`  (
+  `ssxm_dm` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ssxm` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `bz` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `yxbz` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
